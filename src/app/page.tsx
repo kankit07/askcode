4d0814400f67cd348bcceaa104b376d655b39635
k-ankit07 <07.kankit@gmail.com>
2025-01-10 17:03:01 +0530

    initialized project

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+import { LatestPost } from "@/app/_components/post";
+import { auth } from "@/server/auth";
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await auth();
+
+  if (session?.user) {
+    void api.post.getLatest.prefetch();
+  }
+
+  return (
+    <HydrateClient>
+      <h1 className="text-blue-900 text-3xl">hii!</h1>
+    </HydrateClient>
+  );
+}
